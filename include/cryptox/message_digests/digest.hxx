55a01780479115ad7eb78ece415de68943dd7cee
Rodrigo Madera <rodrigo.madera@gmail.com>
2016-11-26 06:50:41 -0200

    Created digest<>() functions and tests.

--- include/cryptox/message_digests/digest.hxx
+++ include/cryptox/message_digests/digest.hxx
@@ -10,14 +10,57 @@
 // [===========================================================================]
 
 #pragma once
+#include "../detail/has_member_trait.hxx"
+#include <boost/mpl/and.hpp>
 #include "message_digester.hxx"
+#include <boost/utility/enable_if.hpp>
 
 namespace cryptox {
 
-	template <class Algorithm, typename Byte>
-	typename Algorithm::digest_type digest(const Byte* data, const size_t size) {
+	namespace detail {
+
+		DEFINE_HAS_MEMBER_TRAIT(begin)
+		DEFINE_HAS_MEMBER_TRAIT(end)
+		DEFINE_HAS_NESTED_TYPE(const_iterator)
+
+		template <class T>
+		struct is_container : public boost::mpl::and_<
+			has_member_begin<T>,
+			has_member_end<T>,
+			has_nested_type_const_iterator<T>
+		> {};
+
+	}
+
+	template <class Algorithm, typename T>
+	typename boost::enable_if_c<
+		sizeof(T) == 1,
+		typename Algorithm::digest_type
+	>::type
+	digest(const T* data, const size_t size) {
 		message_digester<Algorithm> digester;
-		digester.update(data, size);
+		digester.update((const std::uint8_t*)data, size);
+		return digester.digest();
+	}
+
+	template <class Algorithm, typename InputIterator>
+	typename boost::enable_if_c<
+		sizeof(typename InputIterator::value_type) == 1,
+		typename Algorithm::digest_type
+	>::type
+	digest(InputIterator begin, InputIterator end) {
+		// TODO: Read data sequentially.
+		return typename Algorithm::digest_type();
+	}
+
+	template <class Algorithm, typename Container>
+	typename boost::enable_if<
+		detail::is_container<Container>,
+		typename Algorithm::digest_type
+	>::type
+	digest(const Container& container) {
+		message_digester<Algorithm> digester;
+		digester.update((const std::uint8_t*)&container[0], container.size());
 		return digester.digest();
 	}
 
