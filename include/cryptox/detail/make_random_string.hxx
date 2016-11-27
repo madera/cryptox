e12a2932f44aa2980f1be6bdc4a302875b392de4
Rodrigo Madera <rodrigo.madera@gmail.com>
2016-11-27 08:21:18 -0200

    Created internal make_random_string.

---
+++ include/cryptox/detail/make_random_string.hxx
@@ -0,0 +1,33 @@
+// [===========================================================================]
+// [                               S p l o t c h                               ]
+// [---------------------------------------------------------------------------]
+// [                                                                           ]
+// [                          Copyright (C) 2016-2017                          ]
+// [                      Rodrigo Madera <madera@acm.org>                      ]
+// [                                                                           ]
+// [---------------------------------------------------------------------------]
+// [                            All Rights Reserved                            ]
+// [===========================================================================]
+
+#pragma once
+#include <string>
+#include <random>
+
+namespace cryptox {
+
+	namespace detail {
+
+		static inline std::string make_random_string(const size_t size = 32) {
+			const char alphabet[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+			const size_t alphabet_size = sizeof(alphabet) - 1;
+
+			std::string result(chars, ' ');
+			for (char& c : result)
+				c = alphabet[rand()%alphabet_size];
+
+			return result;
+		}
+
+	}
+
+}
