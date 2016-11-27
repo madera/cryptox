e59d7920b04dfdeaa412df2500bdbb8e1d84b5e8
Rodrigo Madera <rodrigo.madera@gmail.com>
2016-11-27 08:35:17 -0200

    Created internal ifstream_size.

---
+++ include/cryptox/detail/ifstream_size.hxx
@@ -0,0 +1,30 @@
+// [===========================================================================]
+// [                               S p l o t c h                               ]
+// [---------------------------------------------------------------------------]
+// [                                                                           ]
+// [                          Copyright (C) 2016-2017                          ]
+// [                      Rodrigo Madera <madera@acm.org>                      ]
+// [                                                                           ]
+// [---------------------------------------------------------------------------]
+// [         Distributed under the Boost Software License, Version 1.0         ]
+// [ Read accompanying LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt ]
+// [===========================================================================]
+
+#pragma once
+#include <fstream>
+
+namespace splotch {
+
+	namespace detail {
+
+		static inline size_t ifstream_size(std::ifstream& ifstream) {
+			const size_t tmp = ifstream.tellg();
+			ifstream.seekg(0, std::ios::end);
+			const size_t result = ifstream.tellg();
+			ifstream.seekg(tmp);
+			return result;
+		}
+
+	}
+
+}
