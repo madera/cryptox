17b9507d419752665eef1f36e482a3dbe75d7246
Rodrigo Madera <rodrigo.madera@gmail.com>
2016-12-04 20:53:04 -0200

    Created benchmarks utility.

---
+++ benchmarks/core/cryptox/message_digests/hash_benchmark.cxx
@@ -0,0 +1,19 @@
+// [===========================================================================]
+// [                               C r y p t o x                               ]
+// [---------------------------------------------------------------------------]
+// [                                                                           ]
+// [                          Copyright (C) 2016-2017                          ]
+// [                      Rodrigo Madera <madera@acm.org>                      ]
+// [                                                                           ]
+// [---------------------------------------------------------------------------]
+// [         Distributed under the Boost Software License, Version 1.0         ]
+// [ Read accompanying LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt ]
+// [===========================================================================]
+
+#include "pch.hxx"
+#include <cryptox/message_digests/hash.hxx>
+using namespace cryptox;
+
+NONIUS_BENCHMARK("md5_hash_benchmark", []{
+	return hash<md5>("asdf");
+})
