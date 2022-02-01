50f69ddc8808ffe037b3b1fb15e61298075127f5
Rodrigo Madera <madera@acm.org>
2022-01-31 23:26:58 -0300

    Create detail/is_decryptor.hxx trait.

---
+++ include/cryptox/detail/is_decryptor.hxx
@@ -0,0 +1,21 @@
+// [===========================================================================]
+// [                               C r y p t o x                               ]
+// [---------------------------------------------------------------------------]
+// [                                                                           ]
+// [                          Copyright (C) 2016-2018                          ]
+// [                      Rodrigo Madera <madera@acm.org>                      ]
+// [                                                                           ]
+// [---------------------------------------------------------------------------]
+// [         Distributed under the Boost Software License, Version 1.0         ]
+// [ Read accompanying LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt ]
+// [===========================================================================]
+
+#pragma once
+#include "is_encryptor.hxx"
+
+namespace cryptox { namespace detail {
+
+	template <class T>
+	struct is_decryptor : boost::false_type {};
+
+}}
